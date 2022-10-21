48acd9cc0727785e5df9627a1d14845edd58fc89
Luis <Luistejeda0011@gmail.com>
2022-10-21 15:58:50 -0300

    Working responsive home design

--- src/components/footerbanner.jsx
+++ src/components/footerbanner.jsx
@@ -2,23 +2,23 @@
 export default function FooterBannerComponent ({ product }) {
   return (
     <div className='flex justify-center'>
-      <div className='py-[100px] px-10 bg-[#f02d34] rounded-2xl relative h-[400px] leading-[1] text-white w-[90%] mt-[120px]'>
+      <div className='py-[100px] px-4 md:px-10 bg-[#f02d34] rounded-2xl relative h-[300px] sm:h-[400px] leading-[1] text-white w-[90%] mt-[120px]'>
         <div className='flex justify-between'>
-          <div className='left'>
+          <div className='left md:w-auto w-44 md:h-48 h-44 flex flex-col sm:justify-between'>
             <p>20% OFF</p>
-            <h3 className='font-black text-[80px] ml-[25px]'>{product[6].largeText}</h3>
-            <h3 className='font-black text-[80px] ml-[25px]'>SMILE</h3>
-            <p className='m-[18px]'>20 OCT to 7 DEC</p>
+            <h3 className='font-black md:mt-4 text-3xl sm:text-[60px] lg:text-[80px] ml-0 lg:ml-[25px]'>{product[6].largeText}</h3>
+            <h3 className='font-black md:mt-8  text-3xl sm:text-[60px] lg:text-[80px] ml-0 lg:ml-[25px]'>SMILE</h3>
+            <p className='text-sm md:mt-8'>20 OCT to 7 DEC</p>
           </div>
-          <div className='leading-[1.4] mt-[-40px] max-w-[400px]'>
+          <div className='leading-[1.4] mt-[-40px] mr-[-40px] md:mr-0 w-64 md:w-auto md:max-w-[250px] lg:max-w-[400px]'>
             <p className='text-lg'>{product[6].smallText}</p>
-            <h3 className='text-6xl font-extrabold'>{product[6].midText}</h3>
-            <p className='text-lg text-justify'>{product[6].description}</p>
-            <button className='rounded-[15px] py-[10px] px-[16px] bg-white text-red-600 border-none mt-10 text-lg font-medium cursor-pointer' type='button'>SHOP NOW</button>
+            <h3 className='lg:text-6xl md:text-4xl text-3xl font-extrabold'>{product[6].midText}</h3>
+            <p className='text-lg text-justify hidden md:block'>{product[6].description}</p>
+            <button className='rounded-[15px] mt-20 py-[10px] px-[16px] bg-white text-red-600 border-none lg:mt-10 text-lg font-medium cursor-pointer' type='button'>SHOP NOW</button>
           </div>
 
           <img
-            src={product[6].img} className='absolute top-[-25%] left-[25%]'
+            src={product[6].img} className='absolute top-[-22%] lg:top-[-25%] left-0 sm:left-[15%] lg:left-[25%]'
           />
         </div>
       </div>
--- src/components/herobanner.jsx
+++ src/components/herobanner.jsx
@@ -1,16 +1,16 @@
 export default function HeroBannerComponent ({ products }) {
   return (
-    <div className='flex justify-center mt-10'>
-      <div className='py-[100px] px-[40px] bg-[#dcdcdc] md:rounded-2xl relative h-[500px] leading-[0.9] w-screen md:w-[90%]'>
+    <div className='flex justify-center mt-5 sm:mt-10'>
+      <div className='py-[100px] px-[30px] sm:px-[40px] bg-[#dcdcdc] md:rounded-2xl relative h-[500px] leading-[0.9] w-screen md:w-[90%]'>
         <p className='text-xl'>{products[0].smallText}</p>
-        <h3 className='text-[4rem] mt-1'>{products[0].midText}</h3>
+        <h3 className='text-[4rem] ml-[-8px] sm:ml-0 mt-1'>{products[0].midText}</h3>
         <h1 className='text-white text-[10rem] ml-[-20px] uppercase'>{products[0].largeText}</h1>
-        <img className='absolute top-0 right-[20%] w-[450px] h-[450px]' src={products[0].img} alt='headphones' />
+        <img className='absolute top-0 right-0 lg:right-[20%] w-[450px] h-[450px]' src={products[0].img} alt='headphones' />
         <div>
           <button className='rounded-2xl py-[10px] px-[16px] bg-[#f02d34] text-white border-none mt-10 text-lg font-medium cursor-pointer z-[10000]' type='button'>Shop {products[0].Product_Name}</button>
           <div className='absolute right-[10%] bottom-[5%] w-[300px] leading-[1.3] flex flex-col text-[#324d67]'>
-            <h5 className='mb-3 font-bold text-base self-end'>Description</h5>
-            <p className='text-[#5f5f5f] font-thin text-end'>{products[0].description}</p>
+            <h5 className='mb-3 font-bold text-base hidden md:block self-end'>Description</h5>
+            <p className='text-[#5f5f5f] font-thin hidden md:block text-end'>{products[0].description}</p>
           </div>
         </div>
       </div>
