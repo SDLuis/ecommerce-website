6acb7312315fdc73ffd9f59ad61236929392b2e2
Luis <Luistejeda0011@gmail.com>
2022-10-21 03:44:57 -0300

    Working Home Page

---
+++ src/components/home.jsx
@@ -0,0 +1,22 @@
+import HeroBannerComponent from './herobanner'
+import UseProducts from '../hooks/useProducts'
+import ProductsComponent from './products'
+import FooterBannerComponent from './footerbanner'
+
+export default function Home () {
+  const { products } = UseProducts()
+  console.log(products)
+  return (
+    <div>
+      {products.length !== 0 ? <HeroBannerComponent products={products.length && products} /> : null}
+      <div className='text-center my-10 mx-0'>
+        <h2 className='text-[40px] font-extrabold'>Best Seller Products</h2>
+        <p className='text-base font-extralight'>speaker There are many variations passages</p>
+      </div>
+      <div className='flex flex-wrap justify-center gap-4 mt-5 w-full'>
+        {products?.map((product) => <ProductsComponent key={product.Product_ID} product={product} />)}
+      </div>
+      {products.length !== 0 ? <FooterBannerComponent product={products} /> : null}
+    </div>
+  )
+}
