09de70d36cab498bf62e46207960f195724c387f
Luis <Luistejeda0011@gmail.com>
2022-10-22 00:50:36 -0300

    Removing hooks and fetching with astro

--- src/components/products.jsx
+++ src/components/products.jsx
@@ -1,19 +1,21 @@
 /* eslint-disable camelcase */
-import React from 'react'
 
-export default function ProductsComponent ({ product: { img, Product_Name, price } }) {
+export default function ProductsComponent ({ product: { img, Product_Name, price, Product_ID } }) {
   return (
     <div>
-      <div className='cursor-pointer text-[#324d67] hover:scale-[1.1] transform transition ease-in-out duration-500'>
-        <img
-          src={img}
-          width={250}
-          height={250}
-          className='rounded-2xl bg-[#ebebeb] scale-[1,1] transform transition ease-in-out duration-500'
-        />
-        <p className='font-medium'>{Product_Name}</p>
-        <p className='font-extrabold mt-[6px] text-black'>${price}</p>
-      </div>
+      <a href={`/products/${Product_ID}`}>
+        <div className='cursor-pointer text-[#324d67] hover:scale-[1.1] transform transition ease-in-out duration-500'>
+          <img
+            alt='products'
+            src={img}
+            width={250}
+            height={250}
+            className='rounded-2xl bg-[#ebebeb] scale-[1,1] transform transition ease-in-out duration-500'
+          />
+          <p className='font-medium'>{Product_Name}</p>
+          <p className='font-extrabold mt-[6px] text-black'>${price}</p>
+        </div>
+      </a>
     </div>
   )
 }
