ba181e060ee685726e2a818b6af0c98182da9361
Luis <Luistejeda0011@gmail.com>
2022-11-02 21:09:37 -0300

    Dinamic routes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config'
 
-// https://astro.build/config
 import react from '@astrojs/react'
-
-// https://astro.build/config
 import tailwind from '@astrojs/tailwind'
+import netlify from '@astrojs/netlify/functions'
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: netlify(),
   integrations: [react(), tailwind()]
 })
