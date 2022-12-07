c7e32cb201542a338c287c6e3279198ede1b225c
Luis <Luistejeda0011@gmail.com>
2022-12-07 14:16:53 -0400

    Changing adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
-import vercel from '@astrojs/vercel/serverless'
+import netlify from '@astrojs/netlify/functions'
 
+// https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
   output: 'server',
-  adapter: vercel()
+  adapter: netlify()
 })
