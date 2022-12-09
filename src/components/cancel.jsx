01ee8d6dcfc844744e3c4f3e24967b9d0e6b603c
Luis <Luistejeda0011@gmail.com>
2022-12-09 01:51:00 -0400

    Center result status components after shopping

--- src/components/cancel.jsx
+++ src/components/cancel.jsx
@@ -1,21 +1,23 @@
 
 const Canceled = () => {
   return (
-    <div className=' bg-white dark:bg-gray-700 h-[60vh]'>
-      <div className='canceled w-[92%] md:w-[1000px] mt-[100px] h-[350px] m-auto p-4 md:p-[50px] rounded-[15px] flex justify-center items-center flex-col bg-red-900'>
-        <h2 className='md:text-[40px] text-2xl capitalize mt-[15px] font-black text-gray-200'>Your order has been canceled</h2>
-        <p className='email-msg text-lg font-semibold mt-5 text-center'>Your products will remain in your cart.</p>
-        <p className='description text-base font-semibold text-center m-[10px] mt-[30px] ml-[5px] text-gray-200 '>
-          If you have any questions, please email
-          <a className='email ml-[5px] text-blue-400' href='mailto:order@example.com'>
-            order@example.com
+    <div className='grid place-items-center py-16 h-[65vh]'>
+      <div className='w-full h-full py-10 bg-white dark:bg-gray-700'>
+        <div className='canceled w-[92%] md:w-[1000px] h-[350px] m-auto p-4 md:p-[50px] rounded-[15px] flex justify-center items-center flex-col bg-red-900'>
+          <h2 className='md:text-[40px] text-2xl capitalize mt-[15px] font-black text-gray-200'>Your order has been canceled</h2>
+          <p className='email-msg text-lg font-semibold mt-5 text-center'>Your products will remain in your cart.</p>
+          <p className='description text-base font-semibold text-center m-[10px] mt-[30px] ml-[5px] text-gray-200 '>
+            If you have any questions, please email
+            <a className='email ml-[5px] text-blue-400' href='mailto:order@example.com'>
+              order@example.com
+            </a>
+          </p>
+          <a href='/'>
+            <button type='button' width='300px' className='btn w-full max-w-[400px] rounded-[15px] border-none py-[10px] px-6 md:px-20 text-xl mt-10 uppercase bg-blue-500 text-white cursor-pointer scale-[1,1] duration-500 transform ease-in-out hover:scale-[1.1]'>
+              Continue Shopping
+            </button>
           </a>
-        </p>
-        <a href='/'>
-          <button type='button' width='300px' className='btn w-full max-w-[400px] rounded-[15px] border-none py-[10px] px-6 md:px-20 text-xl mt-10 uppercase bg-blue-500 text-white cursor-pointer scale-[1,1] duration-500 transform ease-in-out hover:scale-[1.1]'>
-            Continue Shopping
-          </button>
-        </a>
+        </div>
       </div>
     </div>
   )
--- src/components/success.jsx
+++ src/components/success.jsx
@@ -16,24 +16,26 @@ const Success = ({ clear }) => {
   }, [])
 
   return (
-    <div className=' bg-white dark:bg-gray-700 h-[60vh]'>
-      <div className='success w-[92%] md:w-[1000px] mt-[100px] h-[350px] m-auto p-4 md:p-[50px] rounded-[15px] flex justify-center items-center flex-col bg-[#dcdcdc]'>
-        <p className='icon text-[40px]'>
-          <ShoppingSucces />
-        </p>
-        <h2 className='md:text-[40px] text-2xl capitalize mt-[15px] font-black text-[#324d67]'>Thank you for your order!</h2>
-        <p className='email-msg text-base font-semibold mt-5 text-center'>Check your email inbox for the receipt.</p>
-        <p className='description text-base font-semibold text-center m-[10px] mt-[30px] ml-[5px] text-[#f02d34] '>
-          If you have any questions, please email
-          <a className='email ml-[5px] text-[#f02d34]' href='mailto:order@example.com'>
-            order@example.com
+    <div className='grid place-items-center h-[65vh] py-16'>
+      <div className='w-full h-full py-10 bg-white dark:bg-gray-700'>
+        <div className='success w-[92%] md:w-[1000px] h-[350px] m-auto p-4 md:px-[50px] rounded-[15px] flex justify-center items-center flex-col bg-[#dcdcdc]'>
+          <p className='icon text-[40px]'>
+            <ShoppingSucces />
+          </p>
+          <h2 className='md:text-[40px] text-2xl capitalize mt-[15px] font-black text-[#324d67]'>Thank you for your order!</h2>
+          <p className='email-msg text-base font-semibold mt-5 text-center'>Check your email inbox for the receipt.</p>
+          <p className='description text-base font-semibold text-center m-[10px] mt-[30px] ml-[5px] text-[#f02d34] '>
+            If you have any questions, please email
+            <a className='email ml-[5px] text-[#f02d34]' href='mailto:order@example.com'>
+              order@example.com
+            </a>
+          </p>
+          <a href='/'>
+            <button type='button' width='300px' className='btn w-full max-w-[400px] rounded-[15px] border-none py-[10px] px-6 md:px-20 text-xl mt-10 uppercase bg-[#f02d34] text-white cursor-pointer scale-[1,1] duration-500 transform ease-in-out hover:scale-[1.1]'>
+              Continue Shopping
+            </button>
           </a>
-        </p>
-        <a href='/'>
-          <button type='button' width='300px' className='btn w-full max-w-[400px] rounded-[15px] border-none py-[10px] px-6 md:px-20 text-xl mt-10 uppercase bg-[#f02d34] text-white cursor-pointer scale-[1,1] duration-500 transform ease-in-out hover:scale-[1.1]'>
-            Continue Shopping
-          </button>
-        </a>
+        </div>
       </div>
     </div>
   )
